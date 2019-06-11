40e517cc9f99bcbc70b94c69b0c4dee2d5eb935d
Roman Mashkovskiy <ra.mashkovskiy@gmail.com>
2019-06-11 15:30:53 +0300

    update

--- src/store/dog/actions.js
+++ src/store/dog/actions.js
@@ -3,6 +3,6 @@ export const API_CALL_REQUEST = 'API_CALL_REQUEST';
 export const fetchDog = () => ({
     type: API_CALL_REQUEST,
     request: {
-        url: 'https://dog.ceo/api/breeds/image/random',
+        url: '/breeds/image/random',
     },
 });
--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
 import {createRequestInstance, watchRequests} from 'redux-saga-requests';
 import {createDriver} from 'redux-saga-requests-axios';
 
+import {requestInstance} from '../utils/axiosConfig';
+
 export default function* rootSaga() {
-    yield createRequestInstance({driver: createDriver(axios)});
+    yield createRequestInstance({driver: createDriver(requestInstance)});
     yield watchRequests();
 }
